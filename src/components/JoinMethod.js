a02892ef2eaac947370312e6af7df1bba20116ff
sjkim <94kimseongjun@gmail.com>
2023-08-07 14:43:19 +0900

    가입방법

--- src/components/JoinMethod.js
+++ src/components/JoinMethod.js
@@ -3,7 +3,7 @@ const JoinMethod = () => {
       <div className="card">
         <div className="card-body">
           <h5 className="card-title">가입 방법</h5>
-          <p className="card-text">가입 방법 설명</p>
+          <p className="card-text">브랜치 생성 완료 후 안내 예정입니다.</p>
         </div>
       </div>
     );
