a7fab75ee2eb85e854dbabff5c13a4f05c2ae2d2
sjkim <94kimseongjun@gmail.com>
2023-08-07 14:56:03 +0900

    소개 추가

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -5,7 +5,12 @@ const Introduction = () => {
     <div className="card">
       <div className="card-body">
         <h5 className="card-title">맨체스터시티 오피셜서포터즈클럽 MCS</h5>
-        <p className="card-text">맨체스터시티 오피셜서포터즈클럽 MCS 홈페이지에 방문해주셔서 감사합니다.</p>
+        <p className="card-text">맨체스터시티 오피셜서포터즈클럽 MCS 홈페이지에 방문해주셔서 감사합니다.
+        <br></br>
+        <br></br>
+        2023-08-07 서울-강남구 브랜치 생성 요청 완료. 구단의 답변을 기다리는 중 입니다.
+        </p>
+        
       </div>
     </div>
   );
