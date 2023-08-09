de895f33ce88c5473de9cff1c15add05c6059762
94kimseongjun <94kimseongjun@gmail.com>
2023-08-09 18:48:15 +0900

    mci 변경

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -4,11 +4,15 @@ const Introduction = () => {
   return (
     <div className="card">
       <div className="card-body">
-        <h5 className="card-title">맨체스터시티 오피셜서포터즈클럽 MCS</h5>
+        <h5 className="card-title">맨체스터시티 오피셜서포터즈클럽 MCI</h5>
         <p className="card-text">맨체스터시티 오피셜서포터즈클럽 MCS 홈페이지에 방문해주셔서 감사합니다.
         <br></br>
         <br></br>
         2023-08-07 서울-강남구 브랜치 생성 요청 완료. 구단의 답변을 기다리는 중 입니다.
+        <br></br>
+        2023-08-09 서울-강남구 브랜치 생성 요청 거절.
+        <br></br>
+        2023-08-07 인천광역시 브랜치 생성 요청 완료. 구단의 답변을 기다리는 중 입니다.
         </p>
         
       </div>
